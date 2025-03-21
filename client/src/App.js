e3e23cd322ad754487b579d44fea0373f3c6ae6c
Martin Secker <martin@secker.no>
2025-03-21 15:17:37 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,14 @@ import ContactPage from './pages/ContactPage';
 import Admin from './pages/admin';
 // IMPORTER Bifrost i stedet for TicketingIframe:
 import Bifrost from './components/bifrost';
+import Login from './pages/login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/vote" element={<VotePage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/admin" element={<Admin />} />
