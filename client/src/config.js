4e59c9f28d340e3054851c4636d6beef579e245a
MartinSecker <martin.secker@klp.no>
2025-06-19 14:28:51 +0200

    added google login

---
+++ client/src/config.js
@@ -0,0 +1,7 @@
+// client/src/config.js
+
+/**
+ * This file centralizes the API URL for the entire React application.
+ * It reads the URL from the .env file.
+ */
+export const API_URL = process.env.REACT_APP_API_URL;
